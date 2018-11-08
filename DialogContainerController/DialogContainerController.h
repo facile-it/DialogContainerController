6c1fd08afb483067df9bead2b9633c7662cfcc8f
Elviro Rocca <elviro.rocca@facile.it>
2018-11-08 14:32:07 +0100

    Create project

---
+++ DialogContainerController/DialogContainerController.h
@@ -0,0 +1,19 @@
+//
+//  DialogContainerController.h
+//  DialogContainerController
+//
+//  Created by Elviro Rocca on 08/11/2018.
+//  Copyright © 2018 Elviro Rocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DialogContainerController.
+FOUNDATION_EXPORT double DialogContainerControllerVersionNumber;
+
+//! Project version string for DialogContainerController.
+FOUNDATION_EXPORT const unsigned char DialogContainerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DialogContainerController/PublicHeader.h>
+
+
